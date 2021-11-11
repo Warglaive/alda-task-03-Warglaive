c674cf3f4bd225354981a53fc40b847745b644d4
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-11-11 14:06:23 +0100

    Update Queue.java

--- assignment/implementation/src/main/java/asortingservice/Queue.java
+++ assignment/implementation/src/main/java/asortingservice/Queue.java
@@ -3,15 +3,26 @@ package asortingservice;
 import java.util.Iterator;
 
 public class Queue<E> implements sortingservice.Queue<E> {
+    QueueNode<E> firstNode;
+    QueueNode<E> lastNode;
+    int size;
+
+    public Queue() {
+        this.firstNode = null;
+        this.lastNode = null;
+        this.size = 0;
+    }
+
 
     /**
      * enqueue
      *
-     * @param t element to add
+     * @param item element to add
      */
     @Override
-    public void put(E t) {
-
+    public void put(E item) {
+        QueueNode<E> tempNode = new QueueNode<>(item);
+        this.size++;
     }
 
     /**
@@ -26,12 +37,12 @@ public class Queue<E> implements sortingservice.Queue<E> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public long size() {
-        return 0;
+        return this.size;
     }
 
     @Override
