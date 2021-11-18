ae1245e75994cc801742bc071c5eaf3ea5a0f25e
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-11-18 10:18:29 +0100

    SelectionSorter

---
+++ assignment/implementation/src/main/java/asortingservice/SelectionSorter.java
@@ -0,0 +1,16 @@
+package asortingservice;
+
+import sortingservice.Queue;
+import sortingservice.Sorter;
+
+import java.util.Comparator;
+
+public class SelectionSorter implements Sorter {
+    public SelectionSorter(Comparator comparator) {
+    }
+
+    @Override
+    public Queue sort(Queue q) {
+        return null;
+    }
+}
--- assignment/implementation/src/main/java/asortingservice/SortingServices.java
+++ assignment/implementation/src/main/java/asortingservice/SortingServices.java
@@ -1,6 +1,9 @@
 package asortingservice;
 
 import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import sortingservice.Queue;
 import sortingservice.SortKind;
@@ -8,6 +11,11 @@ import sortingservice.Sorter;
 import sortingservice.SortingServiceFactory;
 
 public class SortingServices implements SortingServiceFactory {
+    Map<SortKind, Supplier<Queue>> queueMap =
+            Map.of(SortKind.SELECTION, () -> new QueueImpl());
+
+    Map<SortKind, Function<Comparator, Sorter>> sorterMap = Map.of(SortKind.SELECTION, (comparator)
+            -> new SelectionSorter(comparator));
 
     @Override
     public Queue createPreferredQueue(SortKind forSorter) {
