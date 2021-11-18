35701a60b2c789fa6b9e77aa57e121a8b6013acc
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-11-18 12:50:47 +0100

    Create SelectionSorterTest.java

---
+++ assignment/implementation/src/test/asortingservice/SelectionSorterTest.java
@@ -0,0 +1,13 @@
+package asortingservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SelectionSorterTest {
+
+    @Test
+    void sort() {
+        
+    }
+}
