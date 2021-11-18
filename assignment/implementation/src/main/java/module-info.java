ce5507f044dcc589e11733715a3af8deb6bfaaf3
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-11-18 10:28:36 +0100

    Update module-info.java

--- assignment/implementation/src/main/java/module-info.java
+++ assignment/implementation/src/main/java/module-info.java
@@ -2,5 +2,5 @@ open module asortingservice {
     requires java.logging;
     requires sortingserviceapi;
     uses sortingservice.SortingServiceFactory;
-    provides sortingservice.SortingServiceFactory with asortingservice.SortingServices;
+    provides sortingservice.SortingServiceFactory with asortingservice;
 }
