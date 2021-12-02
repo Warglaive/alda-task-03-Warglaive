0b4bd715eb604c6468391492a866b6145ec5bb8c
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-12-02 12:16:30 +0100

    Update QueueImplTest.java

--- assignment/implementation/src/test/asortingservice/QueueImplTest.java
+++ assignment/implementation/src/test/asortingservice/QueueImplTest.java
@@ -1,10 +1,26 @@
 package asortingservice;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sortingservice.Queue;
+import sortingservice.SortKind;
+import sortingservice.SortingServiceFactory;
 
-public class QueueImplTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class QueueImplTest<E> {
+    QueueImpl<E> selectionQueue;
+    SortingServiceFactory factory;
+
     @BeforeEach
     void setUp() {
+        this.factory = new SortingServices();
+        this.selectionQueue = (QueueImpl<E>) this.factory.createPreferredQueue(SortKind.SELECTION);
+    }
+
+    @Test
+    void constructorTest() {
+        assertThat(this.selectionQueue).isExactlyInstanceOf(QueueImpl.class);
 
     }
 }
