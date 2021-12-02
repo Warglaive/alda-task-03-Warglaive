8486e3cd333e7c9d8579a2f5fd7679b263e5a70d
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-12-02 12:26:39 +0100

    Update QueueImplTest.java

--- assignment/implementation/src/test/asortingservice/QueueImplTest.java
+++ assignment/implementation/src/test/asortingservice/QueueImplTest.java
@@ -21,6 +21,57 @@ public class QueueImplTest<E> {
     @Test
     void constructorTest() {
         assertThat(this.selectionQueue).isExactlyInstanceOf(QueueImpl.class);
+    }
 
+    @Test
+    void getHeadNodeNullTest() {
+        assertThat(this.selectionQueue.getHeadNode()).isNull();
+    }
+
+    @Test
+    void getHeadNodeNotNullTest() {
+        this.selectionQueue.put((E) new Object());
+        assertThat(this.selectionQueue.getHeadNode()).isNotNull();
+    }
+
+    @Test
+    void put() {
+        E item = (E) Integer.valueOf("1");
+        this.selectionQueue.put(item);
+        assertThat(this.selectionQueue.getHeadNode()).isNotNull();
+
+    }
+
+    @Test
+    void get() {
+        E putItem = (E) Integer.valueOf("1");
+        E expectedItem = (E) Integer.valueOf("1");
+        this.selectionQueue.put(putItem);
+        assertThat(this.selectionQueue.get()).usingRecursiveComparison().isEqualTo(expectedItem);
+    }
+
+    @Test
+    void isEmpty() {
+        assertThat(this.selectionQueue.isEmpty()).isTrue();
+        this.selectionQueue.put((E) Integer.valueOf("1"));
+        assertThat(this.selectionQueue.isEmpty()).isFalse();
+    }
+
+    @Test
+    void size() {
+        this.selectionQueue.put((E) Integer.valueOf("1"));
+        long expectedSize = 1;
+        assertThat(this.selectionQueue.size()).isEqualTo(expectedSize);
+    }
+
+    @Test
+    void peek() {
+        this.selectionQueue.put((E) Integer.valueOf("1"));
+        assertThat(this.selectionQueue.peek()).isEqualTo(1);
+    }
+
+    @Test
+    void iterator() {
+        
     }
 }
