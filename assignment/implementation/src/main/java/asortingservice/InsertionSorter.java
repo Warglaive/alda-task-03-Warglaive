c73d5d1c6e4daa49fe24be33cd5cc0fc54a176aa
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-12-05 13:07:57 +0100

    Create InsertionSorter.java

---
+++ assignment/implementation/src/main/java/asortingservice/InsertionSorter.java
@@ -0,0 +1,16 @@
+package asortingservice;
+
+import sortingservice.Queue;
+import sortingservice.Sorter;
+
+public class InsertionSorter<E> implements Sorter<E> {
+    @Override
+    public Queue<E> sort(Queue<E> queue) {
+        return null;
+    }
+
+    @Override
+    public int compareTo(E b) {
+        return 0;
+    }
+}
