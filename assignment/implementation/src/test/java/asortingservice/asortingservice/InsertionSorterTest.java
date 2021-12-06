b6aa6f74add8a5168c490796a5eb8ca0fadba015
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-12-06 17:32:58 +0100

    Update InsertionSorterTest.java

--- assignment/implementation/src/test/java/asortingservice/asortingservice/InsertionSorterTest.java
+++ assignment/implementation/src/test/java/asortingservice/asortingservice/InsertionSorterTest.java
@@ -2,10 +2,13 @@ package asortingservice;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sortingservice.Queue;
 import sortingservice.SortKind;
 import sortingservice.Sorter;
 
 import java.util.Comparator;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class InsertionSorterTest {
 
@@ -22,5 +25,18 @@ public class InsertionSorterTest {
 
     @Test
     void sort() {
+        Queue<Integer> queue = this.factory.createPreferredQueue(SortKind.INSERTION);
+        //hardcoded elementsCount value for test purposes
+        queue = fillRandom(queue, 5);
+        Queue<Integer> sortedQueue = this.sorter.sort(queue);
+        assertThat(sortedQueue).isSameAs(queue);
+    }
+
+    Queue<Integer> fillRandom(Queue<Integer> unsortedQueue, int elementsCount) {
+        //make unsorted queue
+        for (int i = elementsCount; i > 0; i--) {
+            unsortedQueue.put(i);
+        }
+        return unsortedQueue;
     }
 }
