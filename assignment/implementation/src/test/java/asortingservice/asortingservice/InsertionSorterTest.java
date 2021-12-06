003c7bcf7de5d2c57a172c80070e2d9f378fa88d
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-12-06 17:27:31 +0100

    Update InsertionSorterTest.java

--- assignment/implementation/src/test/java/asortingservice/asortingservice/InsertionSorterTest.java
+++ assignment/implementation/src/test/java/asortingservice/asortingservice/InsertionSorterTest.java
@@ -2,11 +2,22 @@ package asortingservice;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sortingservice.SortKind;
+import sortingservice.Sorter;
+
+import java.util.Comparator;
 
 public class InsertionSorterTest {
+
+    private SortingServices factory;
+    private Comparator<Integer> comparator;
+    private Sorter<Integer> sorter;
+
     @BeforeEach
     void setUp() {
-
+        this.factory = new SortingServices();
+        this.comparator = new CountingComparator<>((a, b) -> a.compareTo(b));
+        this.sorter = this.factory.createSorter(SortKind.INSERTION, this.comparator);
     }
 
     @Test
