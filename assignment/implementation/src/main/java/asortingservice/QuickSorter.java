84801a2d551a7a31d7da3c1d1466f3cc9f27456c
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-12-07 11:31:24 +0100

    Create QuickSorter.java

---
+++ assignment/implementation/src/main/java/asortingservice/QuickSorter.java
@@ -0,0 +1,24 @@
+package asortingservice;
+
+import sortingservice.Queue;
+import sortingservice.Sorter;
+
+import java.util.Comparator;
+
+public class QuickSorter<E> implements Sorter<E> {
+    private Comparator<E> comparator;
+
+    public QuickSorter(Comparator<E> comparator) {
+        this.comparator = comparator;
+    }
+
+    @Override
+    public Queue<E> sort(Queue<E> queue) {
+        return null;
+    }
+
+    @Override
+    public int compareTo(E b) {
+        return 0;
+    }
+}
