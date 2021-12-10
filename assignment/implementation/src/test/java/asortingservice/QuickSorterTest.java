875d82dc3965545edd44fe0602f1152f4082a7e9
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-12-10 13:40:06 +0100

    Create QuickSorterTest.java

---
+++ assignment/implementation/src/test/java/asortingservice/QuickSorterTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuickSorterTest {
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void sort() {
+    }
+
+    @Test
+    void partition() {
+    }
+}
