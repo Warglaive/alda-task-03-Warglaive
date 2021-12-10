43627283ae1b964e2e078f744ec3268bb611203a
Svetoslav Stoyanov <426911@student.fontys.nl>
2021-12-10 13:42:42 +0100

    Update QuickSorterTest.java

--- assignment/implementation/src/test/java/asortingservice/QuickSorterTest.java
+++ assignment/implementation/src/test/java/asortingservice/QuickSorterTest.java
@@ -1,14 +1,25 @@
+import asortingservice.CountingComparator;
+import asortingservice.SortingServices;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sortingservice.SortKind;
+import sortingservice.Sorter;
+
+import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuickSorterTest {
-
+    private SortingServices factory;
+    private Comparator<Integer> integerComparator;
+    private Sorter<Integer> integerSorter;
 
     @BeforeEach
     void setUp() {
-
+        this.factory = new SortingServices();
+        //Integer
+        this.integerComparator = new CountingComparator<>(Integer::compareTo);
+        this.integerSorter = this.factory.createSorter(SortKind.INSERTION, this.integerComparator);
     }
 
     @Test
